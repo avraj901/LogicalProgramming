7b40ddcd4824c616799ab3fb0593f8f582f3f276
avraj901 <avraj901@gmail.com>
2025-04-02 12:19:21 +0530

    reverse each word of string

---
+++ LogicalProgramming/src/com/java8/ReverseEachWordOfString.java
@@ -0,0 +1,16 @@
+package com.java8;
+
+import java.util.stream.Stream;
+
+public class ReverseEachWordOfString {
+
+	public static void main(String[] args) {
+
+		String str = "Java is Object Oriented";
+
+
+		Stream.of(str.split(" ")).map(word -> new StringBuilder(word).reverse()).forEach(System.out::println);
+
+	}
+
+}
